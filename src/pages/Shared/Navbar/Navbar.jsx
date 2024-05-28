b53d3a4473e77fd38967f821961a9ee42103fa3b
bijoymamud <bijoymamud.09@gmail.com>
2024-05-28 21:48:20 +0600

    banner section

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -1,10 +1,43 @@
-import React from 'react';
+
 
 const Navbar = () => {
+
+
+  const navOptions = <>
+    <li className="uppercase"><a>Home</a></li>
+    <li>
+      <a className="uppercase">Contact Us</a>
+
+    </li>
+    <li><a className="uppercase">Dashboard</a></li>
+    <li><a className="uppercase">Our Menu</a></li>
+    <li><a className="uppercase">Our Shop</a></li>
+
+  </>
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className="navbar bg-base-100">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+            </div>
+            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+              {navOptions}
+            </ul>
+          </div>
+          <img className="w-[100px]" src="https://i.ibb.co/NV2XnyS/Black-White-Minimalist-Business-Logo-1-removebg-preview.png" alt="" />
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            {navOptions}
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <a className="btn">Button</a>
+        </div>
+      </div>
+    </>
   );
 };
 
