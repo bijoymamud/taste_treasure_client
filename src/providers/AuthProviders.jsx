b98e5fc8fcf3f177c8526bbdaa2e696e07ec33d2
bijoymamud <bijoymamud.09@gmail.com>
2024-06-15 21:36:33 +0600

    JWT Token To Back End For Verification

--- src/providers/AuthProviders.jsx
+++ src/providers/AuthProviders.jsx
@@ -48,8 +48,12 @@ const AuthProviders = ({ children }) => {
       if (currentUser) {
         axios.post('http://localhost:5000/jwt', { email: currentUser.email })
           .then(data => {
-            console.log(data);
+            // console.log(data.data.token);
+            localStorage.setItem('jwt_access_token', data.data.token)
           })
+      }
+      else {
+        localStorage.removeItem('jwt_access_token')
       }
       setLoading(false);
     });
